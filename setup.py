a30611880b134d7aa810c2cb2f0a3b1ec28fbdaa
NDAEDZO AUSTIN MUKHUBA <djtough24@gmail.com>
2025-10-11 13:56:33 +0200

    Fix CI build issues: add setup.py for backwards compatibility and restore .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""
+Setup script for NetSentinel - backwards compatibility with pyproject.toml
+"""
+
+from setuptools import setup
+
+# This setup.py is here for backwards compatibility.
+# All configuration is in pyproject.toml
+setup()
